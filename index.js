4fcb209b686a53104730ff1bc32ea0518082da6f
Rachel Parris <rachel.parris@gmail.com>
2022-05-05 18:53:34 -0500

    Add ability to save watchlist

--- index.js
+++ index.js
@@ -3,29 +3,48 @@ let renderMovies = (movieArray) => {
     let movieHtmlArray = movieArray.map((currentMovie) => {
         let movieTitle = currentMovie.Title;
         let movieYear = currentMovie.Year;
+        let movieId = currentMovie.imdbID;
         let moviePoster = currentMovie.Poster;
 
         return `<div class="movie card col-3">
                 <img src="${moviePoster}" alt="${movieTitle} Image">
                 <h2 class="movie-title card-title">${movieTitle}</h2>
                 <p class="release-date card-text">${movieYear}</p>
-                <button class="add-movie btn btn-primary">Add~</button>
+                <button class="add-button btn btn-primary" data-imdbid=${movieId}>Add!</button>
             </div>`;
     });
 
-    return movieHtmlArray.join("");
+    return movieHtmlArray.join('');
+}
+
+let saveToWatchlist = (movieId) => {
+    let watchlistJSON = localStorage.getItem('watchlist');
+    let watchlist = JSON.parse(watchlistJSON);
+    localStorage.setItem('watchlist', watchlistJSON);
+    
+    let movie = movieData.find((currentMovie) => {
+        return currentMovie.imdbId == movieId;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    // code here will execute after the document is loaded
     const formSubmit = document.getElementById('form-submit');
-    formSubmit.addEventListener('click', function(e){
-        e.preventDefault();
 
+    // TODO: Change to submit event
+    formSubmit.addEventListener('click', function(event){
+        event.preventDefault();
+
+        // Render movie cards to the page
         let render = renderMovies(movieData);
-        document.getElementById("list").innerHTML = render;
+        document.getElementById('list').innerHTML = render;
+    });
+
+    document.addEventListener('click', function(event) {
+        let list = event.target.classList;
+
+        if (list.contains('add-button')) {
+            let movie = event.target.dataset.imdbid;
+            saveToWatchlist(movie);
+        }
     });
 });
-
-
-
